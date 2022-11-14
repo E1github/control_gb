543c6a0b661e23bd4bf6b9c7a8ede90c405e8d2c
e1 <e1@e1.com>
2022-11-15 00:29:19 +0300

    add project with function Entering data from user

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i <= array.Length - 2; i++) Console.Write($"{array[i]},");
+    Console.Write($"{array[array.Length - 1]}]");
+}
+
+
+string[] CreateFullStrArrayFromUser(int rows)
+{
+    string[] strArrFull = new string[rows];
+    
+
+    for (int i = 0; i < strArrFull.GetLength(0); i++) // 0 - rows  0 
+    {
+        Console.Write($"Введите {i+1}-й элемент массива : ");
+        strArrFull[i] = Console.ReadLine();    
+    }
+    return strArrFull;
+}
+
+Console.Write("Введите количество строк: ");
+int rows  = Convert.ToInt32(Console.ReadLine());
+
+string[] strFullArray = CreateFullStrArrayFromUser(rows);
+
+PrintArray(strFullArray);
+
